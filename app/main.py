71d211245bda5c8ab26a6228a6e92750bc625f67
Davesh Sharma <devtherock18@gmail.com>
2026-01-25 21:46:40 +0100

    infinite loop

--- app/main.py
+++ app/main.py
@@ -2,11 +2,12 @@ import sys
 
 
 def main():
-    sys.stdout.write("$ ")
-    pass
+    while True:
+        sys.stdout.write("$ ")
+        pass
+        command = input()
+        print (f"{command}: command not found")
 
-    command = input()
-    print (f"{command}: command not found")
 
 if __name__ == "__main__":
     main()
