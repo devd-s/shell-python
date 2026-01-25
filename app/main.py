ce66ae02ec39b7678e26ad1b9422bd02b8eefa47
Davesh Sharma <devtherock18@gmail.com>
2026-01-25 23:26:42 +0100

    added type

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import sys
 
+builtins_cmds = ["type", "echo", "exit"]
 
 def main():
     while True:
@@ -9,6 +10,11 @@ def main():
         tokens= command.split()
         if command == "exit":
             break
+        elif tokens[0] == "type":
+            if tokens[1] not in builtins_cmds:
+                print (f"{tokens[1]}: not found")
+            else:
+                print(f"{tokens[1]} is a shell builtin")
         elif tokens[0] == "echo":
             print(" ".join(tokens[1:]))
         else:
