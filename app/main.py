62c8bd1f4e20e7b27718a767dbc0ac2397f1ee3b
Davesh Sharma <devtherock18@gmail.com>
2026-01-25 22:49:39 +0100

    breaking

--- app/main.py
+++ app/main.py
@@ -6,7 +6,11 @@ def main():
         sys.stdout.write("$ ")
         pass
         command = input()
-        print (f"{command}: command not found")
+        if command == "exit":
+            break
+        else:
+            print (f"{command}: command not found")
+       
 
 
 if __name__ == "__main__":
