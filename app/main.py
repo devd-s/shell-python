cc3e6fe48e32e0012db9c67e15c3a8422012abac
Davesh Sharma <devtherock18@gmail.com>
2026-02-07 00:17:20 +0100

    adding pipeline support

--- app/main.py
+++ app/main.py
@@ -2,12 +2,62 @@ import sys
 import os
 import shlex
 import readline
+import subprocess
 from pathlib import Path
 
 builtins_cmds = ["type", "echo", "exit"]
 
-# first_tab_pressed = False
-# last_completion_text = None
+def execute_builtins(cmd_tokens, stdin_data=None):
+    if cmd_tokens[0] == "echo":
+        joiner = " ".join(cmd_tokens[1:])
+        output = joiner.replace("\\n", "\n")
+        return output + "\n"
+    elif cmd_tokens[0] == "type":
+        if len(cmd_tokens) < 2:
+            return ""
+        target_cmd = cmd_tokens[1]
+        if target_cmd in builtins_cmds:
+            return (f"{cmd_tokens[1]} is a shell builtin")
+        elif path_exists(cmd_tokens[1]) is not None:
+            return (f"{cmd_tokens[1]} is {path_exists(cmd_tokens[1])}")
+        else:
+            return (f"{cmd_tokens[1]}: not found")
+    return ""
+
+
+def execute_pipeline(command):
+    pipe_segment = command.split("|")
+
+    if len(pipe_segment) == 1:
+        return False
+    stdin_data = None
+
+    for segment in pipe_segment:
+        segment = segment.strip()
+        tokens = shlex.split(segment, posix=True)
+
+        if not tokens:
+            continue
+
+        if tokens[0] in builtins_cmds:
+            output = execute_builtins(tokens, stdin_data)
+            stdin_data = output
+        else:
+            try:
+                if stdin_data is not None:
+                    result = subprocess.run(tokens,input=stdin_data,capture_output=True,text=True)
+                else:
+                    result = subprocess.run(tokens,capture_output=True,text=True)
+
+                stdin_data = result.stdout
+            except FileNotFoundError:
+                print(f"{tokens[0]}: command not found")
+                return True
+            
+    if stdin_data is not None:
+        print(stdin_data, end="")
+    
+    return True
 
 def display_match(substitution, matches, longest_match_length):
     print()
@@ -29,7 +79,6 @@ def bash_complete(text: str, state: int) -> str:
     options_available_executable = [ val + " " for val in find_executables_in_path(text)]
     all_matches=  options_available_executable + options_available_builtin
     return all_matches[state] if state < len(all_matches) else None
-    #return options_available[state] if state < len(options_available) else None
 
 def path_exists(cmd):
     env_path = os.environ.get("PATH", "")
@@ -60,6 +109,8 @@ def main():
 #        executable_cmnd = multi_args[0]
         if command == "exit":
             break
+        elif "|" in command:
+            execute_pipeline(command)
         elif ">" in command:
             os.system(command)
         elif tokens[0] == "type":
