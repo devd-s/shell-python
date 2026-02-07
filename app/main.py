d806121253cd14e6692f6f65934ccb87186e7a77
Davesh Sharma <devtherock18@gmail.com>
2026-02-07 12:56:23 +0100

    adding history details

--- app/main.py
+++ app/main.py
@@ -135,10 +135,12 @@ def main():
     readline.parse_and_bind("tab: complete")
     readline.set_completer(bash_complete)
     readline.set_completion_display_matches_hook(display_match)
+    readline.set_auto_history(False)
     while True:
         sys.stdout.write("$ ")
         pass
         command = input()
+        readline.add_history(command)
         add_to_history(command)
         tokens= shlex.split(command, posix=True)
 #        multi_args = shlex.split(command)
